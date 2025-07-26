988205b783f2c054dcd83622ed1bb7ac1164f540
Teejman17 <teejman17@gmail.com>
2025-07-26 10:20:51 -0600

    W02 Journal updates

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -5,8 +5,9 @@ using System.IO;
 
 public class Journal
 {
-    public List<string> _entry = new List<string>();
+    public List<Entry> _entry = new List<Entry>();
     PromptGenerator GetPrompt = new PromptGenerator();
+
 
     public void AddEntry()
     {
@@ -15,15 +16,17 @@ public class Journal
         Console.WriteLine($"{prompt}");
         Console.Write("> ");
         string entry = Console.ReadLine();
-        _entry.Add(entry);
+        newEntry._date = DateTime.Now;
+        newEntry._promptText = prompt;
+        newEntry._entryText = entry;
+        _entry.Add(newEntry);
     }
 
     public void DisplayAll()
     {
         foreach (var entry in _entry)
-        // foreach (var entry in _entry)
         {
-            Console.WriteLine($"Entry: {entry}");
+            Console.WriteLine($"Entry: {entry.Display}");
         }
 
     }
@@ -52,11 +55,12 @@ public class Journal
     {
         Console.WriteLine("Please enter the file name you would like to load from: ");
         string fileName = Console.ReadLine();
-        _entry.AddRange(System.IO.File.ReadAllLines(fileName));
+        string[] lines = System.IO.File.ReadAllLines(fileName);
 
-        // while ()
-        // {
-
-        // }
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);            
+        }
+        
     }
 }
